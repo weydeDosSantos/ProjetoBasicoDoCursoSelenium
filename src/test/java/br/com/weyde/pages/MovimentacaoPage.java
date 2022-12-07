ed291f68f82d22e66b90ce29e0f820e97944cb01
weydeDosSantos <uede1994@outlook.com>
2022-12-07 09:25:40 -0300

    f5

--- src/test/java/br/com/weyde/pages/MovimentacaoPage.java
+++ src/test/java/br/com/weyde/pages/MovimentacaoPage.java
@@ -1,6 +1,12 @@
 package br.com.weyde.pages;
 
+import static br.com.weyde.core.DriverFactory.getDriver;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 import br.com.weyde.core.BasePage;
 
@@ -21,15 +27,15 @@ public class MovimentacaoPage extends BasePage {
 	public void interessado(String interessado) {
 		escreve("interessado", interessado);
 	}
-	
+
 	public void valor(String valor) {
 		escreve("valor", valor);
 	}
-	
+
 	public void conta(String conta) {
 		selecionarCombo("conta", conta);
 	}
-	
+
 	public void pago() {
 		clicarRadio("status_pago");
 	}
@@ -37,9 +43,18 @@ public class MovimentacaoPage extends BasePage {
 	public void Salvar() {
 		clicarBotaoPorTexto("Salvar");
 	}
-	
+
 	public String obterMensagemSucesso() {
 		return obterTexto(By.xpath("//div[@class='alert alert-success']"));
+	}
+
+	public List<String> obterErros() {
+		   List<WebElement> erros = getDriver().findElements(By.xpath("//div[@class='alert alert-danger']//li"));
+		   List<String> retorno = new ArrayList<String>();
+		   for(WebElement erro: erros) {
+			   retorno.add(erro.getText());
+		   }
+		   return retorno;
 	}
 
 }
--- src/test/java/br/com/weyde/tests/MovimentacaoTest.java
+++ src/test/java/br/com/weyde/tests/MovimentacaoTest.java
@@ -1,5 +1,10 @@
 package br.com.weyde.tests;
 
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -11,7 +16,7 @@ public class MovimentacaoTest extends BaseTest {
 
 	private MenuPage menuPage = new MenuPage();
 	private MovimentacaoPage movPage = new MovimentacaoPage();
-	
+
 	@Test
 	public void testInserirMovimentacao() {
 
@@ -24,10 +29,27 @@ public class MovimentacaoTest extends BaseTest {
 		movPage.conta("Conta para teste alterada2");
 		movPage.pago();
 		movPage.Salvar();
-		
+
 		Assert.assertEquals("Movimentação adicionada com sucesso!", movPage.obterMensagemSucesso());
 
+	}
 
+	@Test
+	public void testCamposObrigatorios() {
+		menuPage.acessarTelaInserirMovimentacaoS();
+
+		movPage.Salvar();
+		List<String> erros = movPage.obterErros();
+		// Assert.assertEquals("Data da Movimentação é obrigatório", erros.get(0));
+		// OPÇÃO 1
+		// Assert.assertTrue(erros.contains("Data da Movimentação é obrigatório"));
+		// OPÇÃO 2
+		Assert.assertTrue(erros.containsAll(Arrays.asList("Data da Movimentação é obrigatório",
+				"Data do pagamento é obrigatório", "Descrição é obrigatório", "Interessado é obrigatório",
+				"Valor é obrigatório", "Valor deve ser um número")));
 
 	}
+	
+	
+
 }
