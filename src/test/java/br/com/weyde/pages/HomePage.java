b71f30df1d29cba1841600439680d893c6f7ad38
weydeDosSantos <uede1994@outlook.com>
2022-12-11 08:52:32 -0300

    subindo

---
+++ src/test/java/br/com/weyde/pages/HomePage.java
@@ -0,0 +1,8 @@
+package br.com.weyde.pages;
+
+import br.com.weyde.core.BasePage;
+
+public class HomePage extends BasePage {
+
+	
+}
---
+++ src/test/java/br/com/weyde/tests/SaldoTest.java
@@ -0,0 +1,7 @@
+package br.com.weyde.tests;
+
+import br.com.weyde.core.BaseTest;
+
+public class SaldoTest extends BaseTest {
+
+}
